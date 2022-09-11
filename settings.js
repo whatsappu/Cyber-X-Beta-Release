cc13a56206afa3d89992742fe6863fe83c2da619
whatsappu <111974881+whatsappu@users.noreply.github.com>
2022-09-11 11:01:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,31 +36,31 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
-global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
-global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+global.vcardowner = ['94761676948'] //ur owner number
+global.ownername = "𝗠𝗥.𝗔𝗥𝗢𝗦𝗛𝗔" //ur owner name
+global.ytname = "YT:𝗠𝗥.𝗔𝗥𝗢𝗦𝗛𝗔" //ur yt chanel name
+global.socialm = "GitHub: 𝗠𝗥.𝗔𝗥𝗢𝗦𝗛𝗔" //ur github or insta name
+global.location = "Sri Lanka, Anuradhapura, Rajanganaya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
+global.owner = ['94761676948']
+global.ownertag = '94761676948' //ur tag number
+global.botnumber = '94761676948' //Add your Bot Number For Log
+global.botname = '𝗠𝗥.𝗔𝗥𝗢𝗦𝗛𝗔💝𝗕𝗢𝗧' //ur bot name
 global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
 global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
 global.linkz = 'https://youtu.be/uzMbW5v9QLE'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
-global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
-global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
-global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.gbyeimg = "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.alivemsg = "ඔබ සතුව නැති දේ ලබා ගැනීමේ පහසුම ක්‍රමය  ඇගයීමයි.. හැකි සෑම විටම තව කෙනෙක්ව ඇගයීමට උත්සහ කරන්න. එය ඔබේ මානසික සංවර්ධිත බවේ සලකුණකි..\nසංස්කරණය කළ පරිවර්තනයකි...\n@best_m_otivation\n#lifetips\n#psychology\n#questions"
+global.packname = "𝗠𝗥.𝗔𝗥𝗢𝗦𝗛𝗔" //ur sticker watermark packname
+global.author = "𝗠𝗥.𝗔𝗥𝗢𝗦𝗛𝗔" //ur sticker watermark author
+global.footer = "® 𝗠𝗥.𝗔𝗥𝗢𝗦𝗛𝗔💝𝗕𝗢𝗧"
+global.aliveimg ="https://blogger.googleusercontent.com/img/a/AVvXsEiMlD0UckXg7GnWJT_QXoQFEk6ME2kxXSR72tCaH-dpQRMlQc_AybgAiNOp9seOLxyaiOfV4fHOW9alPA71OdsoAJl3ad5j1PLpTr9ZQD__VZJr9lZIBOYGAgiPQsjPZaJdV5bq9dRI7gZutcuimSWIOn_VnIJWW6FQ5bYOyhq9lw9DhihpAegP4ROr=s431"
+global.allmenuimg ="https://blogger.googleusercontent.com/img/a/AVvXsEiMlD0UckXg7GnWJT_QXoQFEk6ME2kxXSR72tCaH-dpQRMlQc_AybgAiNOp9seOLxyaiOfV4fHOW9alPA71OdsoAJl3ad5j1PLpTr9ZQD__VZJr9lZIBOYGAgiPQsjPZaJdV5bq9dRI7gZutcuimSWIOn_VnIJWW6FQ5bYOyhq9lw9DhihpAegP4ROr=s431"
+global.welcomeimg= "https://blogger.googleusercontent.com/img/a/AVvXsEiMlD0UckXg7GnWJT_QXoQFEk6ME2kxXSR72tCaH-dpQRMlQc_AybgAiNOp9seOLxyaiOfV4fHOW9alPA71OdsoAJl3ad5j1PLpTr9ZQD__VZJr9lZIBOYGAgiPQsjPZaJdV5bq9dRI7gZutcuimSWIOn_VnIJWW6FQ5bYOyhq9lw9DhihpAegP4ROr=s431"
+global.gbyeimg = "https://blogger.googleusercontent.com/img/a/AVvXsEiMlD0UckXg7GnWJT_QXoQFEk6ME2kxXSR72tCaH-dpQRMlQc_AybgAiNOp9seOLxyaiOfV4fHOW9alPA71OdsoAJl3ad5j1PLpTr9ZQD__VZJr9lZIBOYGAgiPQsjPZaJdV5bq9dRI7gZutcuimSWIOn_VnIJWW6FQ5bYOyhq9lw9DhihpAegP4ROr=s431"
+global.alivemsg = "ඔබ සතුව නැති දේ ලබා ගැනීමේ පහසුම ක්‍රමය  ඇගයීමයි.. හැකි සෑම විටම තව කෙනෙක්ව ඇගයීමට උත්සහ කරන්න. එය ඔබේ මානසික සංවර්ධිත බවේ සලකුණකි..\n𝗔𝗥𝗢𝗦𝗛𝗔💝𝗕𝗢𝗧...\n@best_m_otivation\n#lifetips\n#psychology\n#questions"
 
 //BOT MEDIAS
 global.thum = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
@@ -70,7 +70,7 @@ global.thumb = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
 
 //DATA BASED
 
-global.premium = ['94711421243'] //ADD YOU NUMBER
+global.premium = ['94761676948'] //ADD YOU NUMBER
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -97,12 +97,12 @@ global.prefa = ['','!','.']
 global.sp = '⭔'
 global.mess = {
     success: '✉️✏️ _Done_',
-    admin: '✉️✏️ _This Feature Is Only For Admin!_',
-    botAdmin: '✉️✏️ _Bot Must Be Admin First!_',
+    admin: '✉️✏️ _This Feature Is Only For MR.Arosha!_',
+    botAdmin: '✉️✏️ _Bot Must Be MR.Arosha First!_',
     owner: '✉️✏️ _This Feature Is Only For Owner!_',
     group: '✉️✏️ _Feature Used Only For Groups!_',
     private: '✉️✏️ _Features Used Only For Private Chat!_',
-    bot: '✉️✏️ _This Feature Is Only For Bot_',
+    bot: '✉️✏️ _This Feature Is Only For MR.Arosha_',
     wait: '✉️✏️ _Processing Please Wait..._',
     linkm: '📧✏️ _Where is the link?_',
     error: '✉️🤕 ```Error!!```',
